354d640c1cc8a1c56558ba01af3ba87cb3d236d7
Rezky Azhar A <91133085+RezkyAzhar@users.noreply.github.com>
2022-12-14 11:35:18 +0700

    Activity Log
    spatie

--- app/Models/Buku.php
+++ app/Models/Buku.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Buku extends Model
 {
+    use LogsActivity;
+
     use HasFactory;
     /**
      * The attributes that are mass assignable.
--- app/Models/data_peminjaman.php
+++ app/Models/data_peminjaman.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class data_peminjaman extends Model
 {
+    use LogsActivity;
+
     use HasFactory;
 
     protected $table = 'data_peminjaman';
