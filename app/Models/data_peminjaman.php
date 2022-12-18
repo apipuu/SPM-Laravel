9718030929aa7a3a357afe67ffe432230e4f51bb
Rezky Azhar A <91133085+RezkyAzhar@users.noreply.github.com>
2022-12-18 18:53:17 +0700

    log update

--- app/Models/data_peminjaman.php
+++ app/Models/data_peminjaman.php
@@ -8,7 +8,7 @@ use Spatie\Activitylog\Traits\LogsActivity;
 
 class data_peminjaman extends Model
 {
-    use HasFactory, LogsActivity;
+    use LogsActivity, HasFactory;
 
     protected $table = 'data_peminjaman';
     public $timestamps = false;
@@ -21,4 +21,11 @@ class data_peminjaman extends Model
         'status'
     ];
 
+    protected static $logName = 'Data Peminjaman';
+    protected static $logFillable = true;
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return $this->name . " {$eventName} Oleh: " . Auth::user()->name;
+    }
 }
